7d99b07cccda6e5b8067a74da9aba1b7cd26f941
Justin Jones <hock3ydud3@gmail.com>
2022-04-22 22:35:26 -0500

    rating repo impl

--- src/main/java/com/nachochef/repo/RatingRepository.java
+++ src/main/java/com/nachochef/repo/RatingRepository.java
@@ -1,29 +1,47 @@
 package com.nachochef.repo;
 
 import com.nachochef.domain.Rating;
+import io.micronaut.transaction.annotation.ReadOnly;
+import io.micronaut.transaction.annotation.TransactionalAdvice;
 import jakarta.inject.Singleton;
 
+import javax.persistence.EntityManager;
 import java.util.Optional;
 
 @Singleton
 public class RatingRepository implements Repository<Rating> {
+
+    private EntityManager entityManager;
+
+    public RatingRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
+    @ReadOnly
     public Optional<Rating> findById(long id) {
-        return Optional.empty();
+        return Optional.ofNullable(entityManager.find(Rating.class, id));
     }
 
     @Override
+    @TransactionalAdvice
     public Rating save(Rating objectToSave) {
-        return null;
+        entityManager.persist(objectToSave);
+        return objectToSave;
     }
 
     @Override
+    @TransactionalAdvice
     public void deleteById(long id) {
-
+        findById(id).ifPresent(entityManager::remove);
     }
 
     @Override
+    @TransactionalAdvice
     public int update(Rating objectToUpdate) {
-        return 0;
+        return entityManager.createQuery("update ratings r set rating = :rating where id = :id")
+                .setParameter("rating", objectToUpdate.getRating())
+                .setParameter("id", objectToUpdate.getId())
+                .executeUpdate();
     }
 }
